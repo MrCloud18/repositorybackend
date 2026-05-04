19747e13bf70b47295d6e646ff28c224a952d8dd
MrCloud18 <santiagoyabar18@gmail.com>
2026-05-04 10:58:45 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http')
+
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
+  res.end('Backend Node.js desplegado\n')
+})
+
+server.listen(port, () => console.log(`Server listening on port ${port}`))
