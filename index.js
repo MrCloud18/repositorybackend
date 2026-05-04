3e20b400ee663d5f816ae2d1ecb3b07284dd94ec
MrCloud18 <santiagoyabar18@gmail.com>
2026-05-04 11:34:09 -0500

    Listo para ir a GitHub

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+require('dotenv').config()
 const http = require('http')
 
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
-  res.end('Backend Node.js desplegado\n')
+function requestController(req, res) {
+  console.log('Bienvenidos al curso')
+
+  if (req.url !== '/') {
+    res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' })
+    res.end('Ruta no encontrada\n')
+    return
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+  res.end(`
+    <!doctype html>
+    <html lang="es">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Despliegue01</title>
+      </head>
+      <body>
+        <h1>Bienvenidos al curso</h1>
+        <p>Aplicación corriendo en: ${PORT}</p>
+      </body>
+    </html>
+  `)
+}
+
+const server = http.createServer(requestController)
+
+server.listen(PORT, function () {
+  console.log('Aplicacion corriendo en: ' + PORT)
 })
-
-server.listen(port, () => console.log(`Server listening on port ${port}`))
